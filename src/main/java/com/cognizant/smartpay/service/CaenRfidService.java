6208597ef6fc1b446226769ad5574b3175ad24de
valli <200053623+ValliKasu17@users.noreply.github.com>
2026-02-12 17:01:31 +0530

    Update CaenRfidService.java

--- src/main/java/com/cognizant/smartpay/service/CaenRfidService.java
+++ src/main/java/com/cognizant/smartpay/service/CaenRfidService.java
@@ -14,97 +14,101 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class CaenRfidService {
 
-    private CAENRFIDReader reader;
-    private CAENRFIDLogicalSource source;
-
-    @Autowired
-    private TagForwarder tagForwarder;
-
-    private final Map<String, Long> scannedTags = new ConcurrentHashMap<>();
-
-    @PostConstruct
-    public synchronized void init() {
-        connectReader();
-    }
-
-    private void connectReader() {
-        try {
-            reader = new CAENRFIDReader();
-            // Connecting to the reader
-            reader.Connect(CAENRFIDPort.CAENRFID_RS232, "COM11");
-
-
-            // 🔑 FIX: Wait until reader is fully initialized
-            Thread.sleep(1500);
-            source = reader.GetSources()[0];
-            CAENRFIDReaderInfo info = reader.GetReaderInfo();
-
-            System.out.println("--- Connection Successful ---");
-            System.out.println("Model: " + info.GetModel());
-            System.out.println("Firmware: " + reader.GetFirmwareRelease());
-            // Configuration
-            source.SetReadCycle(1000);
-
-            System.out.println("Starting continuous read... Press Ctrl+C to stop.");
-            System.out.println("-------------------------------------------------");
-
-        } catch (CAENRFIDException e) {
-            System.err.println("RFID Error: " + e.getMessage());
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.err.println("Process interrupted.");
-        }
-    }
-
-    public void startInventory() throws CAENRFIDException {
-        new Thread(() -> {
-            List<String> epcTags = new ArrayList<>();
-            try{
-
-                // Continuous Inventory Loop
-                while (true) {
-                    CAENRFIDTag[] tags = source.InventoryTag();
-
-                    if (tags != null && tags.length > 0) {
-                        for (CAENRFIDTag tag : tags) {
-                            // FIX: Convert the byte ID to Hex String for consistency
-                            String hexId = bytesToHex(tag.GetId());
-                            System.out.println("Tag ID (Hex): " + hexId);
-                            epcTags.add(hexId);
-                        }
-                    }
-                    tagForwarder.forwardTag(epcTags);
-                    epcTags.clear();
-                    // Small sleep to prevent CPU spiking
-                    Thread.sleep(100);
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } catch (CAENRFIDException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-    }
-
-    @PreDestroy
-    public void shutdown() {
-        try {
-            if (source != null) reader.InventoryAbort();/*source.InventoryAbort()*/
-            ;
-            if (reader != null) reader.Disconnect();
-        } catch (Exception ignored) {
-        }
-    }
-
-    // Helper method to convert byte array to a readable Hex String
-    public String bytesToHex(byte[] bytes) {
-        if (bytes == null) return "";
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append(String.format("%02X", b));
-        }
-        return sb.toString();
-    }
+//    private CAENRFIDReader reader;
+//    private CAENRFIDLogicalSource source;
+//
+//    @Autowired
+//    private RFIDScanService RFIDScanService;
+//
+//    @Autowired
+//    private TagForwarder tagForwarder;
+//
+//    private final Map<String, Long> scannedTags = new ConcurrentHashMap<>();
+//
+//    @PostConstruct
+//    public synchronized void init() {
+//        connectReader();
+//    }
+//
+//    private void connectReader() {
+//        try {
+//            reader = new CAENRFIDReader();
+//            // Connecting to the reader
+//            reader.Connect(CAENRFIDPort.CAENRFID_RS232, "COM11");
+//
+//
+//            // 🔑 FIX: Wait until reader is fully initialized
+//            Thread.sleep(1500);
+//            source = reader.GetSources()[0];
+//            CAENRFIDReaderInfo info = reader.GetReaderInfo();
+//
+//            System.out.println("--- Connection Successful ---");
+//            System.out.println("Model: " + info.GetModel());
+//            System.out.println("Firmware: " + reader.GetFirmwareRelease());
+//            // Configuration
+//            source.SetReadCycle(1000);
+//
+//            System.out.println("Starting continuous read... Press Ctrl+C to stop.");
+//            System.out.println("-------------------------------------------------");
+//
+//        } catch (CAENRFIDException e) {
+//            System.err.println("RFID Error: " + e.getMessage());
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//            System.err.println("Process interrupted.");
+//        }
+//    }
+//
+//    public void startInventory() throws CAENRFIDException {
+//        new Thread(() -> {
+//            List<String> epcTags = new ArrayList<>();
+//            try{
+//
+//                // Continuous Inventory Loop
+//                while (true) {
+//                    CAENRFIDTag[] tags = source.InventoryTag();
+//
+//                    if (tags != null && tags.length > 0) {
+//                        for (CAENRFIDTag tag : tags) {
+//                            // FIX: Convert the byte ID to Hex String for consistency
+//                            String hexId = bytesToHex(tag.GetId());
+//                            System.out.println("Tag ID (Hex): " + hexId);
+//                            epcTags.add(hexId);
+//                        }
+//                    }
+//                    RFIDScanService.processScannedTags(epcTags);
+////                    tagForwarder.forwardTag(epcTags);
+//                    epcTags.clear();
+//                    // Small sleep to prevent CPU spiking
+//                    Thread.sleep(100);
+//                }
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            } catch (CAENRFIDException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }).start();
+//    }
+//
+//    @PreDestroy
+//    public void shutdown() {
+//        try {
+//            if (source != null) reader.InventoryAbort();/*source.InventoryAbort()*/
+//            ;
+//            if (reader != null) reader.Disconnect();
+//        } catch (Exception ignored) {
+//        }
+//    }
+//
+//    // Helper method to convert byte array to a readable Hex String
+//    public String bytesToHex(byte[] bytes) {
+//        if (bytes == null) return "";
+//        StringBuilder sb = new StringBuilder();
+//        for (byte b : bytes) {
+//            sb.append(String.format("%02X", b));
+//        }
+//        return sb.toString();
+//    }
 
 }
